b1a95108e4529ad7297364b1e5f3b884e3d5dc7f
Reid Merzbacher <16rmerzbacher@cpsd.us>
2018-03-19 20:24:56 -0400

    map and suggest

--- src/main/java/edu/brown/cs/rmerzbac/main/Main.java
+++ src/main/java/edu/brown/cs/rmerzbac/main/Main.java
@@ -1,11 +1,5 @@
 package edu.brown.cs.rmerzbac.main;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.List;
-
-import edu.brown.cs.rmerzbac.handling.Handling;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 
@@ -17,8 +11,7 @@ import joptsimple.OptionSet;
 public final class Main {
 
   private static final int DEFAULT_PORT = 4567;
-
-  private static final int NUM_INTERSECTIONS = 2;
+  private static REPL repl;
 
   /**
    * The initial method called when execution begins.
@@ -51,91 +44,12 @@ public final class Main {
 
     OptionSet options = parser.parse(args);
 
+    repl = new REPL();
     if (options.has("gui")) {
       System.out.println("gui not set up yet");
       // GUI.runSparkServer((int) options.valueOf("port"));
     }
-    runRepl();
-  }
-
-  /**
-   * Runs the REPL, using a BufferedReader to read user input.
-   */
-  private void runRepl() {
-    try (BufferedReader br = new BufferedReader(
-        new InputStreamReader(System.in))) {
-      String line;
-      while ((line = br.readLine()) != null) {
-        processCommand(line);
-      }
-    } catch (IOException ioe) {
-      // Not possible. No error message can make sense of this.
-      ioe.printStackTrace();
-    }
-  }
-
-  /**
-   * Processes user input and acts accordingly.
-   *
-   * @param line
-   *          The command provided by the user
-   */
-  private void processCommand(String line) {
-    // Split on whitespace and punctuation
-    String[] parsed = line.split("\\s+");
-    if (parsed[0].equals("map")) {
-      if (parsed.length == 2) {
-        System.out.println("map");
-      } else {
-        Handling.improperCommandUse("map <path/to/database>");
-      }
-    } else if (parsed[0].equals("ways")) {
-      if (parsed.length == 5) {
-        System.out.println("ways");
-      } else {
-        Handling.improperCommandUse("ways <lat1> <lon1> <lat2> <lon2>");
-      }
-    } else if (parsed[0].equals("nearest")) {
-      if (parsed.length == 3) {
-        System.out.println("nearest");
-      } else {
-        Handling.improperCommandUse("nearest <latitude> <longitude>");
-      }
-    } else if (parsed[0].equals("suggest")) {
-      System.out.println("suggest");
-    } else if (parsed[0].equals("route")) {
-      int numQuotes = line.length() - line.replace("\"", "").length();
-      if (numQuotes == NUM_INTERSECTIONS * 4) {
-        System.out.println("route w/ names");
-      } else {
-        if (parsed.length == 5 && numQuotes == 0) {
-          System.out.println("route w/ lat lon");
-        } else {
-          Handling.improperCommandUse("route <lat1> <lon1> <lat2> <lon2>");
-        }
-      }
-    } else {
-      Handling.error("Invalid command.");
-      return;
-    }
-  }
-
-  /**
-   * Determines whether an array contains a String.
-   *
-   * @param s
-   *          the string
-   * @param arr
-   *          the array
-   * @return a boolean representing whether the String was found.
-   */
-  public boolean arrayContains(String s, String[] arr) {
-    for (int i = 0; i < arr.length; i++) {
-      if (s.equals(arr[i])) {
-        return true;
-      }
-    }
-    return false;
+    repl.runRepl();
   }
 
 }
