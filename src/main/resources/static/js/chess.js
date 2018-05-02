dcfd1a54e20f474272e8a65ba4e008dfafe246ac
karolynlee <karolynlee@karolyns-air.devices.brown.edu>
2018-05-02 14:23:45 -0400

    revert to old login

--- src/main/resources/static/js/chess.js
+++ src/main/resources/static/js/chess.js
@@ -148,11 +148,7 @@ function setPromotionPiece(piece, coordinates) {
 
 
 function checkCastling(start, end, king) {
-<<<<<<< HEAD
-    if (king === '&#9818' || king === '&#9812') {
-=======
     if (king === "♔" || king === "♚") {
->>>>>>> b3b89fe7ec15abf571d325d2138688be413de092
         if (getRow(start) === getRow(end)) {
             var kingRow = getRow(start);
             var kingCol = getCol(end);
